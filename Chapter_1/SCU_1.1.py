3c13eabf4755f7fb2f3f0dbd9d9efd930c01d197
charliealpha094 <carlosamaral94@gmail.com>
2020-10-25 00:40:11 +0100

    Chapter 1

---
+++ Chapter_1/SCU_1.1.py
@@ -0,0 +1,14 @@
+# Done by Carlos Amaral (15/09/2020)
+
+# SCU 1.1 - Introducing Stop, Code, and Understand! Exercises
+
+
+print("Hey, Taxi!")
+
+print("I would like to go to the airport!")
+
+
+# or
+
+
+print("Hey, Taxi!" + "\nI would like to go to the airport!")
